1a3c8e4be92c06f38ece77b9cc22ec73386e7887
Kennedy Kpogli <kpoglikennedy@gmail.com>
2017-02-09 01:57:47 +0000

    Adding the driver version. Note to self: Remove that marker the app places at previous location.

---
+++ Shuba1/app/src/main/java/com/example/android/shuba1/UserFullName.java
@@ -0,0 +1,18 @@
+package com.example.android.shuba1;
+
+/**
+ * Created by Kennedy on 09/02/2017.
+ */
+
+public class UserFullName {
+
+    public String fullName;
+
+    public UserFullName() {
+
+    }
+
+    public UserFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}

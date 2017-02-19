4fb0064c4c73fd9178bbb1e6fe94f1be0b959799
Kennedy Kpogli <kpoglikennedy@gmail.com>
2017-02-19 01:41:38 +0000

    Structured the chat

--- Shuba1/app/src/main/java/com/example/android/shuba1/CrowdActivity.java
+++ Shuba1/app/src/main/java/com/example/android/shuba1/CrowdActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -43,6 +44,12 @@ public class CrowdActivity extends AppCompatActivity {
         inputMessage = (EditText) findViewById(R.id.crowd_message);
         chatConversation = (TextView) findViewById(R.id.textView);
 
+        buttonSendMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
     }
